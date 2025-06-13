4f1a2c2fbe902122f9ca956aba9cd0e8b3bc1814
Juan Andres Alvarez <juanandres1029@gmail.com>
2025-06-13 16:57:29 -0500

    entry forms whith validators

--- src/app/pages/lot-entry-form/lot-entry-form.component.ts
+++ src/app/pages/lot-entry-form/lot-entry-form.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import {FormGroup, FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-lot-entry-form',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './lot-entry-form.component.html',
   styleUrl: './lot-entry-form.component.css'
 })
 export class LotEntryFormComponent {
+
+  entryForm = signal<FormGroup>(
+    new FormGroup({
+      proveedor: new FormControl('',[Validators.required]),
+      producto: new FormControl('',[Validators.required]),
+      peso: new FormControl('',[Validators.required, Validators.min(0)]),
+      precio: new FormControl('',[Validators.required, Validators.min(1000)]),
+      fecha: new FormControl('',[Validators.required]),
+      factura: new FormControl(''),
+
+    })
+  );
+sendForm(){
+  if(this.entryForm().valid){
+    console.log(this.entryForm().value);
+  }
+  
+}
 
 }
